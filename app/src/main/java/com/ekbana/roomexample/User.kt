18f2590edaf527cf48123a69ea9dfdd1d2e1971b
Ravikant <ravikant@ekbana.com>
2018-12-11 17:56:43 +0545

    Initial commit

---
+++ app/src/main/java/com/ekbana/roomexample/User.kt
@@ -0,0 +1,18 @@
+package com.ekbana.roomexample
+
+import android.arch.persistence.room.Entity
+
+
+/**
+ * @author: RaviKant Paudel
+ * @Email: ravikant.paudel@gmail.com
+ * @LinkedIn: https://www.linkedin.com/in/ravikant-paudel-5b688b8b/
+ * @StackOverflow: https://stackoverflow.com/users/3986391/ravikant-paudel?tab=profile
+ * @Date: 12/11/18
+ */
+@Entity(tableName = "users")
+class User {
+
+//    var id = int
+
+}
